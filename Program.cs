234b83f016b3dea4c7943fe8a7096ad920f14cb4
Gunbacca <albe07001@utb.vaxjo.se>
2024-08-28 11:16:51 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("säg två tal");
+string text = Console.ReadLine();
+string text2 = Console.ReadLine();
+int tal = int.Parse(text);
+int tal2 = int.Parse(text2);
+Console.WriteLine(tal + tal2);
+Console.WriteLine(tal - tal2);
+
+
+/*Console.WriteLine("hej vad är ditt för och efternam");
+string text = Console.ReadLine();
+string text2= Console.ReadLine();
+Console.WriteLine(text +" "+text2);
+*/
+
